764ee066c245f891889ab8284839beec1b55bcf1
ppawan100 <ppawan100@gmail.com>
2022-05-16 15:03:04 +0530

    day6

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
 
+class _LoginPageState extends State<LoginPage> {
+  String userName = "";
+  String password = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -18,7 +24,7 @@ class LoginPage extends StatelessWidget {
               height: 20.0,
             ),
             Text(
-              "Welcome",
+              "Welcome $userName",
               style: TextStyle(
                 fontSize: 22,
                 fontWeight: FontWeight.bold,
@@ -33,6 +39,11 @@ class LoginPage extends StatelessWidget {
               child: Column(
                 children: [
                   TextFormField(
+                    onChanged: (value) {
+                      setState(() {
+                        userName = value;
+                      });
+                    },
                     decoration: InputDecoration(
                       hintText: "Enter userName",
                       labelText: "UserName",
@@ -40,6 +51,11 @@ class LoginPage extends StatelessWidget {
                   ),
                   TextFormField(
                     obscureText: true,
+                    onChanged: (value) {
+                      setState(() {
+                        password = value;
+                      });
+                    },
                     decoration: InputDecoration(
                       hintText: "Enter password",
                       labelText: "Password",
@@ -48,13 +64,51 @@ class LoginPage extends StatelessWidget {
                   SizedBox(
                     height: 20.0,
                   ),
-                  ElevatedButton(
-                    child: Text("Login"),
-                    style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                    onPressed: () {
-                      Navigator.pushNamed(context, AppRoutes.homeRoute);
+                  InkWell(
+                    onTap: () async {
+                      if (userName != "" && password != "") {
+                        setState(() {
+                          changeButton = true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
+                        Navigator.pushNamed(context, AppRoutes.homeRoute);
+                      }
                     },
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: changeButton ? 50 : 150,
+                      height: 50,
+                      alignment: Alignment.center,
+                      child: changeButton
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : Text(
+                              "Login",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                      decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius:
+                              BorderRadius.circular((changeButton ? 50 : 8))),
+                    ),
                   )
+                  // ElevatedButton(
+                  //   child: Text("Login"),
+                  //   style: TextButton.styleFrom(
+                  //     minimumSize: Size(150, 40),
+                  //   ),
+                  //   onPressed: () {
+                  //     if (userName != "" && password != "") {
+                  //       Navigator.pushNamed(context, AppRoutes.homeRoute);
+                  //     }
+                  //   },
+                  // )
                 ],
               ),
             ),
